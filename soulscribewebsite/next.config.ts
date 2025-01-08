09ddc09846469c7caf73e5dc8509a9c1a3a3115e
rjaanav <jaanav.g@gmail.com>
2025-01-08 11:00:43 -0500

    working commit

--- soulscribewebsite/next.config.ts
+++ soulscribewebsite/next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: 'export',
-  basePath: '/soulscribewebsite', // Replace <repository-name> with your GitHub repository name
-  assetPrefix: '/soulscribewebsite', // Ensures assets use the correct path
+  reactStrictMode: true, // Ensures assets use the correct path
 };
 
 module.exports = nextConfig;
