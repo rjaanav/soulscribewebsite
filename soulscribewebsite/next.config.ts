86e45238da40955663e857f73c989a00a7244672
rjaanav <jaanav.g@gmail.com>
2025-01-08 00:36:54 -0500

    new static build commit

--- soulscribewebsite/next.config.ts
+++ soulscribewebsite/next.config.ts
@@ -2,9 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint checks during builds
+    ignoreDuringBuilds: true,
   },
   output: 'export',
+  basePath: '/<repository-name>', // Replace <repository-name> with your GitHub repository name
+  assetPrefix: '/<repository-name>/', // Ensures assets use the correct path
 };
 
 module.exports = nextConfig;
